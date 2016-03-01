3084304f7645d1901f72cd18fe3591b698bd6b9c
Milana Stojadinov <mstojadinov@renderedtext.com>
2016-03-01 22:55:19 +0100

    Use library which skips meta-data etc

--- browser_sim.py
+++ browser_sim.py
@@ -1,13 +1,6 @@
-import socket as request_library
+import urllib as request_library
 
-my_socket = request_library.socket(request_library.AF_INET, request_library.SOCK_STREAM)
-my_socket.connect(("www.py4inf.com", 80))
+response = request_library.urlopen("https://news.ycombinator.com/")
 
-my_socket.send("GET http://www.py4inf.com/code/romeo.txt HTTP/1.0\n\n")
-
-while True:
-    data = my_socket.recv(512)
-    if (len(data) < 1):
-        break
-    print data
-my_socket.close()
+for line in response:
+    print line.strip()
